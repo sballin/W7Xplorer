6304749cd3f8e11ccc572fe77889a05a09abb1fb
Sean Ballinger <sean.ballinger@gmail.com>
2017-09-11 14:45:28 -0400

    add server saving capability

---
+++ cgi-bin/save.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import cgi
+import time
+import glob
+
+fs = cgi.FieldStorage()
+
+saveName = 'datGUIconfig.json'
+
+if glob.glob(saveName):
+    os.rename(saveName, 'datGUIconfig_backup_{}.json'.format(int(time.time())))
+    
+with open(saveName, 'w') as f:
+    f.write(fs.getvalue('text'))
