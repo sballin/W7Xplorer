ddece95c4075dcfc92c9241f3dfe534a338ddd15
Sean Ballinger <sballin@mit.edu>
2017-09-15 15:18:43 -0400

    reorganize project, cgi gives error but works

---
+++ save.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import cgi
+import time
+import glob
+
+fs = cgi.FieldStorage()
+
+saveName = 'datGUIconfig.json'
+
+if glob.glob(saveName):
+    os.rename(saveName, 'backups/datGUIconfig_{}.json'.format(int(time.time())))
+    
+with open(saveName, 'w') as f:
+    f.write(str(fs.getvalue('text')))
+
+os.system('chmod 777 {}'.format(saveName))
+
