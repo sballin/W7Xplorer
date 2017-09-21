bc9a17c4b01bcc423f2152299ece9ffa2db34aa3
Sean Ballinger <sballin@mit.edu>
2017-09-20 20:02:34 -0400

    restore cgi-bin directory

--- cgi-bin/save.py
+++ cgi-bin/save.py
@@ -6,14 +6,10 @@ import time
 import glob
 
 fs = cgi.FieldStorage()
-
-saveName = 'datGUIconfig.json'
-
+saveName = '../datGUIconfig.json'
 if glob.glob(saveName):
-    os.rename(saveName, 'backups/datGUIconfig_{}.json'.format(int(time.time())))
-    
+    os.rename(saveName, '../backups/datGUIconfig_{}.json'.format(int(time.time())))
 with open(saveName, 'w') as f:
     f.write(str(fs.getvalue('text')))
-
 os.system('chmod 777 {}'.format(saveName))
 
